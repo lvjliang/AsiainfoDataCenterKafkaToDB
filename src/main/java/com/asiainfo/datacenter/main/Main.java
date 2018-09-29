f60c99aac2ac7e8f3d7bf19c898c53a82746d37a
18604510128 <dongjb@asiainfo.com>
2018-09-29 08:39:55 +0800

    test

--- src/main/java/com/asiainfo/datacenter/main/Main.java
+++ src/main/java/com/asiainfo/datacenter/main/Main.java
@@ -34,6 +34,5 @@ public class Main {
         oracleEntry.initConfig();
         oracleEntry.startOracleEntry();
         oracleEntry.startKafkaConsumer(bootstrap, topic, groupid, client);
-        System.out.println("dddddd");
     }
 }
